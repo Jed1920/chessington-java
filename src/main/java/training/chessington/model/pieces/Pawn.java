9a7683204c3792f83f06657674030e0f05769cf1
Jed Johnson <jed.johnson@softwire.com>
2019-10-31 15:48:31 +0000

    Red-5 Completed
    Pawns can take another pawn diagonally and will not go off the board diagonally

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -26,26 +26,47 @@ public class Pawn extends AbstractPiece {
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
 
         List<Move> allowedMoves = new ArrayList<>();
+
         if (colour == PlayerColour.WHITE) {
             if (from.getRow() == 0) {
                 return allowedMoves;
             }
-            if ((from.getRow() == START_POS_WHITE) && board.get(from.plus(-2,0))==null){
+            if ((from.getRow() == START_POS_WHITE) && board.get(from.plus(-2, 0)) == null) {
                 allowedMoves.add(new Move(from, from.plus(-2, 0)));
             }
-            if (board.get(from.plus(ONE_STEP_WHITE,0))==null) {
+            if (board.get(from.plus(ONE_STEP_WHITE, 0)) == null) {
                 allowedMoves.add(new Move(from, from.plus(-1, 0)));
+            }
+            if (from.getCol() != 7 && board.get(from.plus(-1, 1)) != null) {
+                if (board.get(from.plus(-1, 1)).getColour() == PlayerColour.BLACK) {
+                    allowedMoves.add(new Move(from, from.plus(-1, 1)));
+                }
+            }
+            if (from.getCol() != 0 && board.get(from.plus(-1, -1)) != null) {
+                if (board.get(from.plus(-1, -1)).getColour() == PlayerColour.BLACK) {
+                    allowedMoves.add(new Move(from, from.plus(-1, -1)));
+                }
             }
 
         } else {
             if (from.getRow() == 7) {
                 return allowedMoves;
             }
-            if ((from.getRow() == START_POSITION_BLACK) && board.get(from.plus(2,0))==null){
+            if ((from.getRow() == START_POSITION_BLACK) && board.get(from.plus(2, 0)) == null) {
                 allowedMoves.add(new Move(from, from.plus(2, 0)));
             }
-            if (board.get(from.plus(ONE_STEP_BLACK,0))==null){
+            if (board.get(from.plus(ONE_STEP_BLACK, 0)) == null) {
                 allowedMoves.add(new Move(from, from.plus(1, 0)));
+            }
+            if (from.getCol() != 7 && board.get(from.plus(1, 1)) != null) {
+                if (board.get(from.plus(1, 1)).getColour() == PlayerColour.WHITE) {
+                    allowedMoves.add(new Move(from, from.plus(1, 1)));
+                }
+            }
+            if (from.getCol() != 0 && board.get(from.plus(1, -1)) != null) {
+                if (board.get(from.plus(1, -1)).getColour() == PlayerColour.WHITE) {
+                    allowedMoves.add(new Move(from, from.plus(1, -1)));
+                }
             }
         }
         return allowedMoves;
