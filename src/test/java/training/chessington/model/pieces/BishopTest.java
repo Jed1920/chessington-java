5276655505541cd3284072d4b51e4abf093b2bc8
Jed Johnson <jed.johnson@softwire.com>
2019-11-03 13:44:15 +0000

    Bishop - Test 3
    Bishop Can take diagonally piece of another colour

--- src/test/java/training/chessington/model/pieces/BishopTest.java
+++ src/test/java/training/chessington/model/pieces/BishopTest.java
@@ -81,7 +81,7 @@ public class BishopTest {
     }
 
     @Test
-    public void rookCannotMoveOverPiece() {
+    public void bishopCannotMoveOverPiece() {
         Board board = Board.empty();
 
         Piece whiteBishop = new Bishop(PlayerColour.WHITE);
@@ -114,68 +114,44 @@ public class BishopTest {
         assertThat(whiteRookMoves).doesNotContain(new Move(whiteBishopCoords, whiteBishopCoords.plus(2, 6)));
     }
 
-//
-//    @Test
-//    public void rookCannotMoveOverPieceHorizontally() {
-//        Board board = Board.empty();
-//
-//        Piece whiteRook = new Rook(PlayerColour.WHITE);
-//        Coordinates whiteRookCoords = new Coordinates(4, 4);
-//        board.placePiece(whiteRookCoords,whiteRook);
-//
-//        Piece whitePawn1 = new Pawn(PlayerColour.WHITE);
-//        Coordinates whitePawnCoords1 = new Coordinates(4, 6);
-//        board.placePiece(whitePawnCoords1, whitePawn1);
-//
-//        Piece whitePawn2 = new Pawn(PlayerColour.WHITE);
-//        Coordinates whitePawnCoords2 = new Coordinates(4, 1);
-//        board.placePiece(whitePawnCoords2, whitePawn2);
-//
-//        // Act
-//        List<Move> whiteRookMoves = whiteRook.getAllowedMoves(whiteRookCoords, board);
-//
-//        // Assert
-//        assertThat(whiteRookMoves).doesNotContain(new Move(whiteRookCoords, whiteRookCoords.plus(0, 3)));
-//        assertThat(whiteRookMoves).doesNotContain(new Move(whiteRookCoords, whiteRookCoords.plus(0, -4)));
-//    }
-//
-//    @Test
-//    public void whiteRookCanCaptureBlackPiece(){
-//        Board board = Board.empty();
-//
-//        Piece whiteRook = new Rook(PlayerColour.WHITE);
-//        Coordinates whiteRookCoords = new Coordinates(4, 4);
-//        board.placePiece(whiteRookCoords,whiteRook);
-//
-//        Piece blackPawn = new Pawn(PlayerColour.BLACK);
-//        Coordinates blackPawnCoords = new Coordinates(4, 6);
-//        board.placePiece(blackPawnCoords, blackPawn);
-//
-//        // Act
-//        List<Move> whiteRookMoves = whiteRook.getAllowedMoves(whiteRookCoords, board);
-//
-//        // Assert
-//        assertThat(whiteRookMoves).contains(new Move(whiteRookCoords, whiteRookCoords.plus(0, 2)));
-//        assertThat(whiteRookMoves).doesNotContain(new Move(whiteRookCoords, whiteRookCoords.plus(0, 3)));
-//    }
-//
-//    @Test
-//    public void blackRookCanCaptureWhitePiece(){
-//        Board board = Board.empty();
-//
-//        Piece blackRook = new Rook(PlayerColour.BLACK);
-//        Coordinates blackRookCoords = new Coordinates(4, 4);
-//        board.placePiece(blackRookCoords,blackRook);
-//
-//        Piece whitePawn = new Pawn(PlayerColour.WHITE);
-//        Coordinates whitePawnCoords = new Coordinates(4, 6);
-//        board.placePiece(whitePawnCoords, whitePawn);
-//
-//        // Act
-//        List<Move> blackRookMoves = blackRook.getAllowedMoves(blackRookCoords, board);
-//
-//        // Assert
-//        assertThat(blackRookMoves).contains(new Move(blackRookCoords, blackRookCoords.plus(0, 2)));
-//        assertThat(blackRookMoves).doesNotContain(new Move(blackRookCoords, blackRookCoords.plus(0, 3)));
-//    }
+
+    @Test
+    public void whiteBishopCanCaptureBlackPiece(){
+        Board board = Board.empty();
+
+        Piece whiteBishop = new Bishop(PlayerColour.WHITE);
+        Coordinates whiteBishopCoords = new Coordinates(4, 4);
+        board.placePiece(whiteBishopCoords,whiteBishop);
+
+        Piece blackPawn = new Pawn(PlayerColour.BLACK);
+        Coordinates blackPawnCoords = new Coordinates(6, 6);
+        board.placePiece(blackPawnCoords, blackPawn);
+
+        // Act
+        List<Move> whiteBishopMoves = whiteBishop.getAllowedMoves(whiteBishopCoords, board);
+
+        // Assert
+        assertThat(whiteBishopMoves).contains(new Move(whiteBishopCoords, whiteBishopCoords.plus(2, 2)));
+        assertThat(whiteBishopMoves).doesNotContain(new Move(whiteBishopCoords, whiteBishopCoords.plus(3, 3)));
+    }
+
+    @Test
+    public void blackBishopCanCaptureWhitePiece(){
+        Board board = Board.empty();
+
+        Piece blackBishop = new Bishop(PlayerColour.BLACK);
+        Coordinates blackBishopCoords = new Coordinates(4, 4);
+        board.placePiece(blackBishopCoords,blackBishop);
+
+        Piece whitePawn = new Pawn(PlayerColour.WHITE);
+        Coordinates whitePawnCoords = new Coordinates(6, 6);
+        board.placePiece(whitePawnCoords, whitePawn);
+
+        // Act
+        List<Move> blackBishopMoves = blackBishop.getAllowedMoves(blackBishopCoords, board);
+
+        // Assert
+        assertThat(blackBishopMoves).contains(new Move(blackBishopCoords, blackBishopCoords.plus(2, 2)));
+        assertThat(blackBishopMoves).doesNotContain(new Move(blackBishopCoords, blackBishopCoords.plus(3, 3)));
+    }
 }
